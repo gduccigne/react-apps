2428f575c9c97fba65faa940f2717ada48951b33
Higa <gduccigne@gmail.com>
2021-02-01 19:42:17 -0300

    Aula final backend

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,9 +1,11 @@
 const { values } = require("lodash");
 const { model } = require("mongoose");
 const BillingCycle = require("./billingCycle");
+const errorHandler = require("../common/errorHandler");
 
 BillingCycle.methods(["get", "post", "put", "delete"]);
 BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.after("post", errorHandler).after("put", errorHandler);
 
 BillingCycle.route("count", (req, res, next) => {
   BillingCycle.count((error, value) => {
